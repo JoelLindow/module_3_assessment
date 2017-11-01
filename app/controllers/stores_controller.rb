fd82221ce8db2fd98874d66e7f787cda741fd449
JoelLindow <joellindow@gmail.com>
2017-11-01 10:48:54 -0600

    Dream Driving Pushing API call to service and returning single line controller with collection of stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,14 @@
 class StoresController < ApplicationController
 
   def index
-    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,phone&pageSize=10&apiKey=#{ENV["Best_Buy_Key"]}"
+    @stores = StoresService.new(params[:zip])
+    # 
+    # response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,phone&pageSize=10&apiKey=#{ENV["Best_Buy_Key"]}"
+    #
+    # raw_stores = JSON.parse(response.body)
 
-    raw_stores = JSON.parse(response.body)
 
-    binding.pry
+    # binding.pry
   end
 
 end
