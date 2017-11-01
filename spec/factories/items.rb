bcd921a5fa17a832ff59123bba9472f620f31715
JoelLindow <joellindow@gmail.com>
2017-11-01 11:32:56 -0600

    built item factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name "Awesome Item"
+    description "This is an awesome Item"
+    image_url "http://brooklynmakers.com/wp-content/uploads/2013/01/snuggly-ugly-monster-kiki.jpg"
+  end
+end
