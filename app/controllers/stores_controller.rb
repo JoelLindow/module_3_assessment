1ceb46cd24d52e13ef3278a88a2d2d8e69e01944
JoelLindow <joellindow@gmail.com>
2017-11-01 10:54:37 -0600

    Due to hangups at the start, not refactoring controller until project completed. MUST WAIT

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,10 +2,10 @@ class StoresController < ApplicationController
 
   def index
     @stores = StoresService.new(params[:zip])
-    # 
-    # response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,phone&pageSize=10&apiKey=#{ENV["Best_Buy_Key"]}"
-    #
-    # raw_stores = JSON.parse(response.body)
+    
+    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,phone&pageSize=10&apiKey=#{ENV["Best_Buy_Key"]}"
+
+    raw_stores = JSON.parse(response.body)
 
 
     # binding.pry
---
+++ app/services/stores_service.rb
@@ -0,0 +1,9 @@
+class StoresService
+
+  def initialize(zip)
+
+  end
+
+
+
+end
